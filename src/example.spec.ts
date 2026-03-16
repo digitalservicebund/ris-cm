182dc99604b58fc081dffb0d7ccd59240c284a41
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2026-03-16 17:40:36 +0100

    Use vitest for tests
    RISDEV-11238

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,9 +1,10 @@
 import { screen } from "@testing-library/dom"
+import { describe, expect, it } from "vitest"
 
 describe("App", () => {
   it("shows Hello ds", () => {
     document.body.innerHTML = `
-      <h1>TypeScript + Vite Application Template<h1>
+      <h1>TypeScript + Vite Application Template<h1>npm 
     `
     expect(
       screen.getByText("TypeScript + Vite Application Template"),
---
+++ vitest.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig, mergeConfig } from "vitest/config"
+import viteConfig from "./vite.config"
+
+export default defineConfig((context) =>
+  mergeConfig(
+    viteConfig(context),
+    defineConfig({
+      test: {
+        setupFiles: ["src/vitest-setup.ts"],
+        globals: true,
+        environment: "jsdom",
+        include: ["src/**/*.spec.ts"],
+        css: {
+          // Needed so we can reliably test for class names for CSS modules.
+          // Otherwise scoped CSS classes would have an unreliable hash
+          // attached to the class name.
+          modules: { classNameStrategy: "non-scoped" },
+        },
+        coverage: {
+          provider: "istanbul",
+          reporter: ["lcov"],
+          // Changes to this also need to be reflected in the sonar-project.properties
+          exclude: [
+            // Configuration and generated outputs
+            "**/[.]**",
+            "coverage/**/*",
+            "dist/**/*",
+            "**/.*rc.{?(c|m)js,yml}",
+            "*.config.{js,ts}",
+
+            // Types
+            "**/*.d.ts",
+
+            // Tests
+            "test/**/*",
+            "e2e/**/*",
+            "a11y/**/*",
+            "src/vitest-setup.ts",
+
+            // App content we're not interested in covering with unit tests. If you
+            // add something here, please also add a comment explaining why the
+            // exclusion is necessary.
+
+            // If necessary to use e.g. guards, we'll have a router-guards file that
+            // then should be tested
+            "src/router.ts",
+
+            // Just the init file and global setup, nothing much to test here.
+            "src/main.ts",
+          ],
+        },
+      },
+    }),
+  ),
+)
