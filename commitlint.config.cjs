d606148d3685c5696bcc3068b53abb4c2c450956
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2026-03-16 12:23:36 +0100

    Enforce issue references in commits
    RISDEV-11238

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -6,4 +6,9 @@ module.exports = {
     "header-full-stop": [2, "never", "."],
     "header-max-length": [2, "always", 50],
   },
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: ["RISDEV-"],
+    },
+  },
 }
