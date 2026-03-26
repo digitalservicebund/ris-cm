e2bb6ca5dd1bdce029ae39de36b06b86266c16cb
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2026-03-26 14:23:51 +0100

    Add sentry sourcemap upload & release creation
    RISDEV-11343

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import vue from "@vitejs/plugin-vue"
 import { fileURLToPath, URL } from "node:url"
 import icons from "unplugin-icons/vite"
 import { defineConfig } from "vite"
+import { sentryVitePlugin } from "@sentry/vite-plugin"
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -15,6 +16,12 @@ export default defineConfig(() => {
       icons({
         scale: 1.3333, // ~24px at the current default font size of 18px
         compiler: "vue3",
+      }),
+      sentryVitePlugin({
+        org: "digitalservice",
+        project: "ris-cm",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        telemetry: false,
       }),
     ],
     resolve: {
